c78f52d7017909a94ad32cac8e29f2cc87297701
Youssef Meskini <youss.meskini@gmail.com>
2024-09-03 17:07:24 +0100

    fix path

--- gatsby-node.ts
+++ gatsby-node.ts
@@ -1,56 +1,45 @@
 import type { GatsbyNode } from "gatsby";
 import path from "path";
 
-require("dotenv").config({
-  path: `.env.${process.env.NODE_ENV}`,
-});
-
 export const createPages: GatsbyNode["createPages"] = async ({
   actions,
   graphql,
 }) => {
-  try {
-    const { createPage } = actions;
-    const sectionTemplate = path.resolve("src/templates/section.tsx");
+  console.log(actions);
+  const { createPage } = actions;
+  const sectionTemplate = path.resolve(__dirname, "src/templates/section.tsx");
 
-    console.log("CREATING PAGES");
-
-    const { data } = await graphql<Queries.AllCoursesQuery>(`
-      query AllCourses {
-        allContentfulCourse {
-          edges {
-            node {
-              title
-              description
-              sections {
-                slug
-              }
+  const { data } = await graphql<Queries.AllCoursesQuery>(`
+    query AllCourses {
+      allContentfulCourse {
+        edges {
+          node {
+            title
+            description
+            sections {
+              slug
             }
           }
         }
       }
-    `);
-
-    console.log(data);
-
-    if (data) {
-      data?.allContentfulCourse.edges.forEach(({ node }) => {
-        if (node?.sections) {
-          node.sections.forEach((section) => {
-            if (section) {
-              createPage({
-                component: sectionTemplate,
-                path: `/${section.slug}`,
-                context: {
-                  slug: section.slug,
-                },
-              });
-            }
-          });
-        }
-      });
     }
-  } catch (err) {
-    console.log(err);
+  `);
+
+  if (data) {
+    data?.allContentfulCourse.edges.forEach(({ node }) => {
+      if (node?.sections) {
+        node.sections.forEach((section) => {
+          if (section) {
+            createPage({
+              component: sectionTemplate,
+              path: `/${section.slug}`,
+              context: {
+                slug: section.slug,
+              },
+            });
+          }
+        });
+      }
+    });
   }
 };
