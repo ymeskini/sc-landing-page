f40a9b4edbcc6e17aab946d3a6f0ed46519ad848
Youssef Meskini <youss.meskini@gmail.com>
2024-09-03 16:56:17 +0100

    trying to debug deploy

--- gatsby-node.ts
+++ gatsby-node.ts
@@ -1,47 +1,56 @@
 import type { GatsbyNode } from "gatsby";
 import path from "path";
 
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+});
 
 export const createPages: GatsbyNode["createPages"] = async ({
   actions,
   graphql,
 }) => {
-  const { createPage } = actions;
-  const sectionTemplate = path.resolve("src/templates/section.tsx");
+  try {
+    const { createPage } = actions;
+    const sectionTemplate = path.resolve("src/templates/section.tsx");
 
-  const { data } = await graphql<Queries.AllCoursesQuery>(`
-    query AllCourses {
-      allContentfulCourse {
-        edges {
-          node {
-            title
-            description
-            sections {
-              slug
+    console.log("CREATING PAGES");
+
+    const { data } = await graphql<Queries.AllCoursesQuery>(`
+      query AllCourses {
+        allContentfulCourse {
+          edges {
+            node {
+              title
+              description
+              sections {
+                slug
+              }
             }
           }
         }
       }
+    `);
+
+    console.log(data);
+
+    if (data) {
+      data?.allContentfulCourse.edges.forEach(({ node }) => {
+        if (node?.sections) {
+          node.sections.forEach((section) => {
+            if (section) {
+              createPage({
+                component: sectionTemplate,
+                path: `/${section.slug}`,
+                context: {
+                  slug: section.slug,
+                },
+              });
+            }
+          });
+        }
+      });
     }
-  `);
-
-  console.log(data);
-
-  if (data) {
-    data?.allContentfulCourse.edges.forEach(({ node }) => {
-      if (node?.sections) {
-        node.sections.forEach((section) => {
-          if (section) {
-            createPage({
-              component: sectionTemplate,
-              path: `/${section.slug}`,
-              context: {
-                slug: section.slug,
-              },
-            });
-          }
-        });
-      }
-    });
+  } catch (err) {
+    console.log(err);
   }
 };
